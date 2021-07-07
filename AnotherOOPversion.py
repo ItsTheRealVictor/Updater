e1b30405aa1ba06e34b51051eabcb6ed262597d1
ItsTheRealVictor <74051392+ItsTheRealVictor@users.noreply.github.com>
2021-07-07 15:14:15 -0700

    Update AnotherOOPversion.py
    Adds a while loop to be able to add more lots and parts. The use can save 1 lot and quit, save multiple lots, or immediately quit.

--- AnotherOOPversion.py
+++ AnotherOOPversion.py
@@ -1,3 +1,4 @@
+from calendar import c
 import openpyxl as opx
 import pprint
 import datetime
@@ -44,7 +45,7 @@ class GetDataFromUser:
 
     dataList = []
 
-    def getVariables(self):
+    def getVariables():
         userLotNumber = q.text('Enter the Lot number: ')
         userPartNumber = q.text('Enter the part number: ')
         userNumOfLots = q.text('Enter the number of lots: ')
@@ -54,45 +55,50 @@ class GetDataFromUser:
         userDateInput = q.text('Enter your start date (YYYY-MM-DD): ')
         userTimeIntervalInput = q.text('enter your time interval: ')
 
-        varList = [userLotNumber, userPartNumber, userNumOfLots,
-           userQuantity, userStartTime, userLotOwner,
-           userDateInput, userTimeIntervalInput]
+        varList = [userLotNumber, #(project) 
+                   userPartNumber, #(product)
+                   userNumOfLots, #(# of lots)
+                   userQuantity, #(qty)
+                   userDateInput, #(date in)
+                   userStartTime, 
+                   userLotOwner,
+                   userTimeIntervalInput]
 
         for var in varList:
             item = var.ask()
             GetDataFromUser.dataList.append(item)
 
-    def storeData(self):
+    def storeData():
         """Stores the data input by the user into a list (dataList)"""
         for index, variable in enumerate(GetDataFromUser.dataList):
             GetDataFromUser.dataList[index] = variable
 
-    def computeDate(self, incomingDate):
+    def computeDate(incomingDate):
         """Takes the date and time interval from the user and computes the future date and day. """
         userTimeIntervalDelta = relativedelta.relativedelta(
             hours=int(GetDataFromUser.dataList[-1])) # I don't know how to access the userTimeIntervalInput variable from the local
                                                      # scope of the function getVariables(). My workaround is to access it by slicing
                                                      # out of the list I created in the for loop at the end of getVariables()
         parsedUserDateInput = parser.parse(
-            GetDataFromUser.dataList[6]).date() # Same as above, the item at dataList[6] is the user's input date. 
-        future = parsedUserDateInput + userTimeIntervalDelta
-        parsedFutureDate = datetime.datetime.strftime(future, "%Y-%m-%d")
+            GetDataFromUser.dataList[4]).date() # Same as above, the item at dataList[4] is the user's input date. 
+        future = parsedUserDateInput + userTimeIntervalDelta #takes the user's date and time interval, adds them together
+        parsedFutureDate = datetime.datetime.strftime(future, "%Y-%m-%d")#converts the future date into datetime format
         parsedUserDateInputDay = datetime.datetime.strftime(
             parsedUserDateInput, "%A")
-        parsedFutureDay = datetime.datetime.strftime(future, "%A")
-        GetDataFromUser.dataList.append(
-            f"{parsedFutureDate} ({parsedFutureDay})")
+        parsedFutureDay = datetime.datetime.strftime(future, "%A") #takes the future date from parsedFutureDate and computes the day 
+                                                                   #of the week
+        GetDataFromUser.dataList.insert(5, f"{parsedFutureDate} ({parsedFutureDay})") #adds the future date and day of the week to the dataList
         return f"{parsedFutureDate}" + f" {parsedFutureDay}"
 
-    def dataSummary(self, ):
+    def dataSummary():
         """Gives the user a summary of the data they have enterd"""
         print(
             f"\nHere is a summary of the data you have entered \n{[i for i in GetDataFromUser.dataList]}")
 
-    def doAllFunctions(self):
-        GetDataFromUser.getVariables(self)
-        GetDataFromUser.storeData(self)
-        GetDataFromUser.computeDate(self, GetDataFromUser.dataList[6])
+    def doAllFunctions():
+        GetDataFromUser.getVariables()
+        GetDataFromUser.storeData()
+        GetDataFromUser.computeDate(GetDataFromUser.dataList[6])
         # GetDataFromUser.dataSummary()
 
 
@@ -125,7 +131,7 @@ class updateSpreadSheet:
                         ]
 
 
-    def pickChamber(self):
+    def pickChamber():
 
         chamberDict = {k: v for k, v in enumerate(updateSpreadSheet.chamberList)} # I used enumerate in case I want to add or subtract chambers in the future.
         pprint.pprint(chamberDict)
@@ -134,30 +140,45 @@ class updateSpreadSheet:
             if userChamberSelection == k:
                 updateSpreadSheet.chamberChoice = f"{v}"
 
-    inputDataList = [i for i in GetDataFromUser.dataList]
-    inputDataList.insert(0, "")
-    inputDataList.insert(1, "")
-    insertLocation = 3
-    for index, value in enumerate(chambersOnlySheetsList):
-        if value == chamberChoice:
-            wb[sheetsList[index]].insert_rows(insertLocation)
-            for i in range(2, 10):
-                cellref = wb[sheetsList[index]].cell(row=3, column=i)
-                cellref.value = inputDataList[i]
+        inputDataList = [i for i in GetDataFromUser.dataList]
+        inputDataList.insert(0, "")
+        inputDataList.insert(1, "")
+        insertLocation = 3
+        for index, value in enumerate(chambersOnlySheetsList):
+            if value == updateSpreadSheet.chamberChoice:
+                wb[sheetsList[index]].insert_rows(insertLocation)
+                for i in range(2, 10):
+                    cellref = wb[sheetsList[index]].cell(row=3, column=i)
+                    cellref.value = inputDataList[i]
+
 
 class confirmUpdateSpreadsheet:
 
+ 
+
     def confirmUpdate():
         CHOICES = ['Update my spreadsheet and quit', 'Update my spreadsheet and add more parts', 'Quit']
-        confirm = q.select("What would you like to do now?", choices = CHOICES).ask()
-        if confirm == 'Update my spreadsheet and quit':
-            wb.save(r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx")
-            sys.exit()
-        elif confirm == 'Update my spreadsheet and add more parts':
-            wb.save(r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx")
-
-        elif confirm == 'Quit':
-            print("Bruh")
-            wb.save(r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx")
+        while True:
+            confirm = q.select("What would you like to do now?", choices = CHOICES).ask()
+            if confirm == 'Update my spreadsheet and quit':
+                wb.save(r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx")
+                sys.exit()
+            elif confirm == 'Update my spreadsheet and add more parts':
+                wb.save(r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx")
+                GetDataFromUser.dataList.clear()
+                GetDataFromUser.doAllFunctions()
+                updateSpreadSheet.pickChamber()
+                keepGoing = q.select("Do you want to keep going?", choices = ['Yes, keep going', 'Quit']).ask()
+                if keepGoing == 'Yes, keep going':
+                    wb.save(r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx")
+                elif keepGoing == 'Quit':
+                    wb.save(r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx")
+                    sys.exit()
+            elif confirm == 'Quit':
+                print("Bruh")
+                sys.exit()
+                
 
 GetDataFromUser.doAllFunctions()
+updateSpreadSheet.pickChamber()
+confirmUpdateSpreadsheet.confirmUpdate()
