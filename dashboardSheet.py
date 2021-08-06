e967fded29fa69acdd025ad6672758620bad4b59
ItsTheRealVictor <74051392+ItsTheRealVictor@users.noreply.github.com>
2021-08-06 14:23:31 -0700

    Update dashboardSheet.py
    I've got solid individual data frames for each chamber now. 
    Still left to do is
             Figure out how to enter each dataframe into a corresponding row/column in the dashboard sheet.
             Paste this code into the main updater, add something like a "view dashboard" option in the opening screen. You'll
             be able to pick which chamber you want to view the dataframe of, or view all df's at the same time.

--- dashboardSheet.py
+++ dashboardSheet.py
@@ -8,10 +8,9 @@ import pandas as pd
 
 wb = opx.Workbook()
 # reference workbook, where data is pulled
-refWB = opx.load_workbook(
-    r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx", data_only=True)
+refWB = opx.load_workbook(r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx", data_only=True)
 # indices of the various chamber-specific sheets
-refSheets = refWB.worksheets[1:15]
+refSheets = refWB.worksheets[0:14]
 activeSheet = wb.active
 activeSheet.title = "Dashboard"
 
@@ -107,25 +106,53 @@ for index, sheet in enumerate(refSheets): #Gathers the lists of and puts it into
     chamberDict[f'{sheet.title}'].append(getData(index, "O")) # % of time remaining
 
 
-for sheet in refSheets:
-    (chamberDict[f'{sheet.title}'][-1]) = [ f'{round(x * 100, 1)}%' for x in (chamberDict[f'{sheet.title}'][-1])]
-
-zippedData = zip(chamberDict['125C Bake'])
+for sheet in refSheets: #converts the % of time remaining into a percentage, rounding off the first decimal place
+    (chamberDict[f'{sheet.title}'][-1]) = [ f'{(round(x * 100, 1))}%' for x in (chamberDict[f'{sheet.title}'][-1])]
 
 
-
-#This works somewhat as intended, using the pandas method insert(). I can make this into a function.
+    
+#columns for the individual dataframes    
 columnList = ["RA Number", "Date/Time in", "Removal Date/Time","Time Until Removal", r"% of time remaining"]
-df = pd.DataFrame()
-df.insert(0, "RA Number", value=chamberDict['125C Bake'][0])
-# df.insert(1, "Date/Time in", value =chamberDict['125C Bake'][1])
-
-print(chamberDict)
-# print(df)
 
 
+def makeDataFrame(chamberSelection):
+    """Makes a data frame for each chamber"""
+    df = pd.DataFrame()
+    df['RA Number'] = pd.Series(chamberDict[f'{chamberSelection}'][0])
+    df['Date/Time in'] = pd.Series(chamberDict[f'{chamberSelection}'][1])
+    df['Removal Date/Time'] = pd.Series(chamberDict[f'{chamberSelection}'][2])
+    df['Time Until Removal'] = pd.Series(chamberDict[f'{chamberSelection}'][3])
+    df[r'% remaining'] = pd.Series(chamberDict[f'{chamberSelection}'][4])
+    return(df)
+
+
+# Each chambers' dataframe. Maybe there is a more elegant way to do this, for now it works. 
+
+bake125CDF = makeDataFrame('125C Bake')
+bake150CDF = makeDataFrame('150C Bake')
+bake180CDF = makeDataFrame('180C Bake')
+bake210CDF = makeDataFrame('210C Bake')
+soak3060DF = makeDataFrame('30.60 Soak')
+soak6060DF = makeDataFrame('85.85 Soak')
+coldOven8DF = makeDataFrame('Oven 8 COLD')
+uHast1DF = makeDataFrame('UHAST 1')
+uHast2DF = makeDataFrame('UHAST 2')
+uHast5DF = makeDataFrame('UHAST 5')
+TC2DF = makeDataFrame('TC.2_D')
+TC3DF = makeDataFrame('TC.3_D')
+TC4DF = makeDataFrame('TC.4_D')
+
+# A list of all the dataframes
+dfList = [bake125CDF, bake150CDF, bake180CDF, 
+          bake210CDF, soak3060DF, soak6060DF,
+          coldOven8DF, uHast1DF, uHast2DF,
+          uHast5DF, TC2DF, TC3DF, TC4DF]
 
 
 wb.save(r"C:\Users\VD102541\Desktop\DashboardTest.xlsx")
 
-
+#I've got solid individual data frames for each chamber. 
+#Still left to do is
+        # Figure out how to enter each dataframe into a corresponding row/column in the dashboard sheet.
+        # Paste this code into the main updater, add something like a "view dashboard" option in the opening screen. You'll
+        # be able to pick which chamber you want to view the dataframe of, or view all df's at the same time. 
