f1a64182ff6188ef13b4bc9438d49686cfee67ce
ItsTheRealVictor <74051392+ItsTheRealVictor@users.noreply.github.com>
2021-08-06 21:10:09 -0700

    Rename to RelWizard.py
    Beginning to add "view dashboard" functionality to opening screen.

--- RelWizard.py
+++ RelWizard.py
@@ -9,12 +9,11 @@ import sys
 import questionary as q
 import pandas as pd
 
-wb = opx.load_workbook(r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx")
+wb = opx.load_workbook(r"C:\Users\valex\Desktop\VictorChamberUsageDummyData.xlsx")
 # # List of sheets
 sheetsList=wb.sheetnames
 chambersOnlySheetsList=sheetsList[0:14]
-writer = pd.ExcelWriter('TestFart.xlsx', engine='openpyxl')
-writer.book = wb
+
 
 
 
@@ -37,12 +36,12 @@ class GetDataFromUser:
             userDateInput = (str(datetime.date.today()))
         userTimeIntervalInput = input('enter your time interval: ')
 
-        varList = [userLotNumber, #(project) 
+        varList = [userLotNumber, #(project)
                    userPartNumber, #(product)
                    userNumOfLots, #(# of lots)
                    userQuantity, #(qty)
                    userDateInput, #(date in)
-                   userStartTime, 
+                   userStartTime,
                    userLotOwner,
                    userTimeIntervalInput]
 
@@ -60,12 +59,12 @@ class GetDataFromUser:
             hours=int(GetDataFromUser.dataList[-1])) # I don't know how to access the userTimeIntervalInput variable from the local
                                                      # scope of the function getVariables(). My workaround is to access it by slicing
                                                      # out of the list I created in the for loop at the end of getVariables()
-        parsedUserDateInput = parser.parse(GetDataFromUser.dataList[4]).date() # Same as above, the item at dataList[4] is the user's input date. 
+        parsedUserDateInput = parser.parse(GetDataFromUser.dataList[4]).date() # Same as above, the item at dataList[4] is the user's input date.
         future = parsedUserDateInput + userTimeIntervalDelta #takes the user's date and time interval, adds them together
         parsedFutureDate = datetime.datetime.strftime(future, "%Y-%m-%d")#converts the future date into datetime format
         parsedUserDateInputDay = datetime.datetime.strftime(
             parsedUserDateInput, "%A")
-        parsedFutureDay = datetime.datetime.strftime(future, "%A") #takes the future date from parsedFutureDate and computes the day 
+        parsedFutureDay = datetime.datetime.strftime(future, "%A") #takes the future date from parsedFutureDate and computes the day
                                                                    #of the week
         GetDataFromUser.dataList.insert(5, f"{parsedFutureDate}") #adds the future date and day of the week to the dataList
         return f"{parsedFutureDate}"
@@ -84,7 +83,7 @@ class updateSpreadSheet:
 
     #chamberChoice is an empty variable because the choice has yet to made by the user
     chamberChoice = ''
-        
+
     chamberList = [
         "125C Bake",
         "150C Bake",
@@ -152,18 +151,18 @@ class confirmUpdateSpreadsheet:
         while True:
             confirm = q.select("What would you like to do now?", choices = CHOICES).ask()
             if confirm == 'Update my spreadsheet and quit':
-                wb.save(r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx")
+                wb.save(r"C:\Users\valex\Desktop\VictorChamberUsageDummyData.xlsx")
                 sys.exit()
             elif confirm == 'Update my spreadsheet and add more parts':
-                wb.save(r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx")
+                wb.save(r"C:\Users\valex\Desktop\VictorChamberUsageDummyData.xlsx")
                 GetDataFromUser.dataList.clear()
                 GetDataFromUser.doAllFunctions()
                 updateSpreadSheet.pickChamber()
                 keepGoing = q.select("Do you want to keep going?", choices = ['Yes, keep going', 'Quit']).ask()
                 if keepGoing == 'Yes, keep going':
-                    wb.save(r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx")
+                    wb.save(r"C:\Users\valex\Desktop\VictorChamberUsageDummyData.xlsx")
                 elif keepGoing == 'Quit':
-                    wb.save(r"C:\Users\VD102541\Desktop\VictorChamberUsage.xlsx")
+                    wb.save(r"C:\Users\valex\Desktop\VictorChamberUsageDummyData.xlsx")
                     sys.exit()
             elif confirm == 'Quit':
                 print("Bruh")
@@ -179,6 +178,7 @@ class OpeningScreen:
                                     Version 1.0
                                     by Victor Delgado
                                     ---------
+                                    Press 'b' to view the dashboard
                                     Press 'u' to use the updater
                                     Press 'd' to use the date calculator
                                     Press 'q' to quit
@@ -193,12 +193,12 @@ class OpeningScreen:
                 userTime = input("Enter your time interval (in hours): ")
 
                 """Takes the date and time interval from the user and computes the future date and day. """
-                userinputTime = relativedelta.relativedelta(hours=int(userTime)) 
-                parsedUserDateInput = parser.parse(startDate).date()  
-                future = parsedUserDateInput + userinputTime 
+                userinputTime = relativedelta.relativedelta(hours=int(userTime))
+                parsedUserDateInput = parser.parse(startDate).date()
+                future = parsedUserDateInput + userinputTime
                 parsedFutureDate = datetime.datetime.strftime(future, "%Y-%m-%d")#converts the future date into datetime format
                 parsedUserDateInputDay = datetime.datetime.strftime(parsedUserDateInput, "%A")
-                parsedFutureDay = datetime.datetime.strftime(future, "%A") #takes the future date from parsedFutureDate and computes the day 
+                parsedFutureDay = datetime.datetime.strftime(future, "%A") #takes the future date from parsedFutureDate and computes the day
                                                                    #of the week
                     # GetDataFromUser.dataList.insert(5, f"{parsedFutureDate} ({parsedFutureDay})") #adds the future date and day of the week to the dataList
                 print(f"{userTime} hours after {parsedUserDateInputDay} {startDate} is {parsedFutureDay} {parsedFutureDate}")
@@ -207,7 +207,10 @@ class OpeningScreen:
             else:
                 print("Invalid input. Try Again")
 
-                
+    def viewDashBoard():
+        pass
+
+
 OpeningScreen.opening()
 GetDataFromUser.doAllFunctions()
 updateSpreadSheet.pickChamber()
